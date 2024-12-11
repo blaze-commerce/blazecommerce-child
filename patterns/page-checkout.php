eac056d507379297dc0ea38be2be6ed2018dba79
Lan <136666489+lanz-2024@users.noreply.github.com>
2024-12-11 19:23:50 +0800

    Add patterns files

---
+++ patterns/page-checkout.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: page-checkout
+ * Slug: twentytwentyfour/page-checkout
+ * Inserter: no
+ */
+?>
+<!-- wp:generateblocks/container {"uniqueId":"144aa67c","align":"full","isDynamic":true,"blockVersion":4,"display":"flex","justifyContent":"center","className":"blz-header"} -->
+<!-- wp:generateblocks/container {"uniqueId":"ad6457e0","isDynamic":true,"blockVersion":4,"justifyContent":"center","typography":{"textAlign":"center"},"spacing":{"paddingTop":"12.5px","paddingBottom":"17.5px"}} -->
+<!-- wp:image {"lightbox":{"enabled":false},"sizeSlug":"full","linkDestination":"custom","align":"center"} -->
+<figure class="wp-block-image aligncenter size-full"><a href="https://tunafishtackle-bc-v1.blz.onl/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Trophy-Tackle-Logo-blue-transparent-website-2048x273-2.png" alt="" class=""/></a></figure>
+<!-- /wp:image -->
+<!-- /wp:generateblocks/container -->
+<!-- /wp:generateblocks/container -->
+
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group">
+				
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php esc_html_e('Checkout', 'twentytwentyfour');?></h1>
+<!-- /wp:heading -->
+				[woocommerce_checkout]
+			</div>
+<!-- /wp:group -->
+
+<!-- wp:generateblocks/container {"uniqueId":"211ca6ff","align":"full","isDynamic":true,"blockVersion":4,"display":"flex","alignItems":"center","sizing":{"height":"94px"},"className":"blaze-custom-footer"} -->
+<!-- wp:generateblocks/container {"uniqueId":"3f8cc872","isDynamic":true,"blockVersion":4,"sizing":{"maxWidth":"1466px","width":"100%"},"spacing":{"marginLeft":"auto","marginRight":"auto","marginTop":"0px","marginBottom":"0px"}} -->
+<!-- wp:generateblocks/grid {"uniqueId":"561c2b25","columns":2,"isDynamic":true,"blockVersion":3} -->
+<!-- wp:generateblocks/container {"uniqueId":"725b7047","isGrid":true,"gridId":"561c2b25","isDynamic":true,"blockVersion":4,"sizing":{"width":"50%","widthMobile":"100%"},"typography":{"textAlign":"left"},"className":"blaze-foot-links"} -->
+<!-- wp:html -->
+<div class="blz-checkout-footer">
+ <p>Copyright © 2024 TunaFishTackle | <a href="/privacy/" class="blz-footer-links">Privacy Policy</a> | <a href="/terms-of-use/" class="blz-footer-links">Terms & Conditions</a>
+</div>
+<!-- /wp:html -->
+<!-- /wp:generateblocks/container -->
+
+<!-- wp:generateblocks/container {"uniqueId":"2a9db318","isGrid":true,"gridId":"561c2b25","isDynamic":true,"blockVersion":4,"sizing":{"width":"50%","widthMobile":"100%"},"typography":{"textAlign":"right"},"className":"blaze-commerce"} -->
+<!-- wp:html -->
+<div> 
+<p> Store by Blaze Commerce.
+</div>
+<!-- /wp:html -->
+<!-- /wp:generateblocks/container -->
+<!-- /wp:generateblocks/grid -->
+<!-- /wp:generateblocks/container -->
+<!-- /wp:generateblocks/container -->
